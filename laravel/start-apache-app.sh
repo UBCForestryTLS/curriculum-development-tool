a2c9f528cee7fe207e7b326f98fc2e5c6dcf8e92
FabianLCH <45581810+FabianLCH@users.noreply.github.com>
2026-08-17 13:57:38 -0700

    Remove interpolated variables from compose file, update Dockerfile to build Laravel app image without env variables and update start script

--- laravel/start-apache-app.sh
+++ laravel/start-apache-app.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+set -e
 
+# optimization step, needs to be run here to access the container's
+# environment
+php artisan config:cache
+
+# run migrations in isolated mode in case of distributed deployment
 php artisan migrate --isolated
 
-# TODO: Create log file for queue and redirect output to docker?
 # start the queue worker as a background process
-php artisan queue:work &
+# logs will be sent to the laravel log file
+php artisan queue:work -n &
 
 # start the apache process in the foreground
 apache2-foreground
